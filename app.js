f82fafa57c5b09f6461be4fcec25eabec9e071fa
Yevheniy Gandzyuck <evgeniy.gandzyuck@area9.dk>
2019-09-27 11:37:59 +0300

    test login with dumb user

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 
 const express = require('express');
 const app = express();
-const passport = require('passport');
+
+const passport = require('passport'),
+      LocalStrategy = require('passport-local'),
+      bodyParser = require('body-parser');
       
 const User = require('./models/user');
 
@@ -16,22 +19,44 @@ app.use(require('express-session')({
   resave: false, 
   saveUninitialized: false,
 }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // passport
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate));
 passport.serializeUser(User.serialize);
 passport.deserializeUser(User.deserialize);
 
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+// Routes (temporary - just to test auth)
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/secret', (req, res) => {
+app.get('/secret', isLoggedIn, (req, res) => {
   res.render('secret');
 });
 
-// TODO: use process.env values
+// Auth routes
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', 
+  passport.authenticate('local', {
+    successRedirect: '/secret',
+    failureRedirect: '/login'
+  })
+);
+
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
   console.log('Server started...');
 })
--- models/user.js
+++ models/user.js
@@ -4,11 +4,24 @@ const User = () => {
   const tmpName = 'barbedwire';
   const tmpPass = '12345';
   return {
+    authenticate: (username, password, done) => {
+      if (username != tmpName) {
+        return done(null, false, { message: 'Incorrect user name'});
+      }
+      if (password != tmpPass) {
+        return done(null, false, { message: 'Incorrect password'});
+      }
+      return done(null,  {
+        id: 1,
+        name: tmpName,
+        passowrd: tmpPass
+      });
+    },
     serialize: (user, done) => {
       done(null, user.id)
     },
     deserialize: (id, done) => {
-      done(err, {
+      done(null, {
         id: 1,
         name: tmpName,
         passowrd: tmpPass
