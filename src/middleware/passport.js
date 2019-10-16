f3a6adfe23dac2c5c742587474cbb99846de9b01
vsevolod gandzyuck <v.gandzyuck@aisnovations.com>
2019-10-16 09:25:50 +0300

    updates

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -5,32 +5,30 @@ const UserModel = require('../models/user');
 
 const serialize = ({ id }, done) => done(null, id);
 const deserialize = (user, done) => done(null, user);
+const authenticate = (db) => async (email, password, done) => {
+  const user = await UserModel(db).getUserByEmail(email);
+
+  if (!user) {
+    return done(null, false, { message: 'Incorrect user name' });
+  }
+
+  if (password !== user.passowrd) {
+    return done(null, false, { message: 'Incorrect password' });
+  }
+
+  return done(null,  {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+  });
+};
 
 module.exports = (app, db) => {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  passport.use(new LocalStrategy(
-    async (email, password, done) => {
-      const user = await UserModel(db).getUserByEmail(email);
-
-      if (!user) {
-        return done(null, false, { message: 'Incorrect user name' });
-      }
-
-      if (password !== user.passowrd) {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-
-      return done(null,  {
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-      });
-    },
-  ));
-
+  passport.use(new LocalStrategy(authenticate(db)));
   passport.serializeUser(serialize);
   passport.deserializeUser(deserialize);
 };
